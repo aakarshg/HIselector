1dbf55524f285c0599e0e0665e52639a3cc8af1e
satvikshetty04 <satvikshetty04@gmail.com>
2017-10-10 14:22:24 -0400

    Updated regex file

--- Satvik/Newspaper_trial.py
+++ Satvik/Newspaper_trial.py
@@ -1,11 +1,19 @@
+'''
+This code returns results that contains a figure for insurance plans.
+A more refined version would include particular keywords from our truth table like deductible, copay, etc.
+'''
+
 import newspaper
 import google
 import re
 
+# Different plans as a test
 plans = ['student blue health insurance', 'PSI health insurance', 'ISO health insurance']
 for p in plans:
     search_results = google.search(p, stop=4, lang="en")
     print("*"*30)
+    print(p.upper())
+    print("_"*15)
     for link in search_results:
         data = newspaper.Article(url=link)
         data.download()
