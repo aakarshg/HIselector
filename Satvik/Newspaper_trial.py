ce60596168b8ed85b3646e6bd2306cbe1ac1cb76
satvikshetty04 <satvikshetty04@gmail.com>
2017-11-12 14:33:54 -0500

    Updated db

--- Satvik/Newspaper_trial.py
+++ Satvik/Newspaper_trial.py
@@ -7,68 +7,74 @@ import newspaper
 import google
 import pandas as pd
 import re
+import time
 
-# Different plans as a test
-pl = pd.read_csv("C:\\Users\\satvi\\Documents\\DDDM\\Project\\IssuerID_Name.csv")
+pl = pd.read_csv("C:\\Users\\satvi\\Documents\\DDDM\\Project\\IssuerID_Name_Updated.csv")
 issuers = pd.read_csv("C:\\Users\\satvi\\Documents\\DDDM\\Project\\Plan_Attributes_PP2.csv", usecols=[1])
+
 issuer_ids = issuers['IssuerId'].tolist()
 pl = pl[pl['IssuerID'].isin(issuer_ids)]
-plans = pl['Issuer_Name'].tolist()
+IssuerName = pl['Issuer_Name'].tolist()
 idList = pl['IssuerID'].tolist()
-plans = [name + ' health insurance reviews' for name in plans]
+plans = [name + ' usinsuranceagents.com health insurance reviews' for name in IssuerName]
 count = 0
-plans_covered = 0
-
-number_of_companies_reviewed = 0
-total_companies = 0
-indexInPlans = 0;
-
-# f = open( "output.csv" , 'w', encoding="latin")
 comments = []
 
-for p in plans[0:3]:
-    # f.write(str( idList[indexInPlans] ) + ", ")
+for p in plans:
     plan_comm = []
-    indexInPlans += 1
-    total_companies += 1
-
-    search_results = google.search(p, stop=4, lang="en")
+    search_results = google.search(p, stop=4, lang="en", pause=3.0, user_agent = 'Mozilla/4.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
     print("*"*30)
     print(p.upper())
     print("_"*15)
     iterated = 0
     for link in search_results:
-        if ( ("www.bbb.org" not in link) and ("usinsuranceagents.com" not in link)):
+        if "usinsuranceagents.com" not in link:
             continue
         try:
             data = newspaper.Article(url=link)
             data.download()
+            time.sleep(1)
             data.parse()
         except:
             continue
 
         text = data.text
+        # print("Entire text "+ text)
         linesOfText = text.split('\n')
-
-        if( "usinsuranceagents.com" in link):
-            for line in linesOfText:
-                if( "found the following review helpful." in line):
-                    if (iterated == 0):
-                        number_of_companies_reviewed += 1
-                        iterated = 1
-                    filtered = re.sub("[0-9]+ of [0-9]+ people found the following review helpful.", "", line)
-                    filtered = filtered.replace("Help others find the most helpful reviews Was this review helpful to you? Yes | No", "")
-                    # filtered = filtered.replace(",", " ")
-                    filtered = filtered.replace("\n", ". ")
-                    filtered = filtered.replace("\"", " ")
-                    filtered = filtered.replace("\'", " ")
-                    filtered = filtered.replace("[", " ")
-                    filtered = filtered.replace("]", " ")
-                    if filtered.strip() != "":
-                        plan_comm.append(filtered)
-                        print(filtered + '\n')
+        next_line = False
+        for line in linesOfText:
+            # if "found the following review helpful." in line:
+            if re.search("(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?"
+                         "|Oct(ober)?|Nov(ember)?|Dec(ember)?),\s+20\d{2}", line):
+                filtered = re.sub("(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?"
+                                  "|Oct(ober)?|Nov(ember)?|Dec(ember)?),\s+20\d{2}", "", line)
+                filtered = re.sub(r'[0-9]+ of [0-9]+ people found the following review helpful.', "", filtered)
+                filtered = filtered.replace("Help others find the most helpful reviews Was this review helpful to you? Yes | No", "")
+                filtered = filtered.replace("\n", ". ").replace("\"", "").replace("\'", "").replace("[", " ").\
+                    replace("]", " ").strip()
+                filtered = re.sub("\s\s+", " ", filtered)
+                if filtered.strip() != "" and filtered.strip() != "Health Insurance,":
+                    plan_comm.append(filtered.capitalize())
+                    print(filtered + '\n')
+                iterated += 1
+            elif re.search(r'[0-9]+ of [0-9]+ people found the following review helpful.', line):
+                next_line = True
+            elif next_line and line.strip() != "":
+                filtered = re.sub("Help others find the most helpful reviews", "", line)
+                filtered = re.sub("\s\s+", " ", filtered)
+                filtered = filtered.replace("\n", ". ").replace("\"", "").replace("\'", "").replace("[", " "). \
+                    replace("]", " ").strip()
+                print(filtered)
+                if filtered.strip() != "" and filtered.strip() != 'Health Insurance,':
+                    plan_comm.append(filtered.strip())
+                iterated += 1
+                next_line = False
+        if iterated>0:
+            break;
     comments.append(plan_comm)
+    count += 1
+    print(count)
+    search_results.close()
 
-df = pd.DataFrame(data={'IssuerID':idList[0:3], 'Reviews':comments})
-df.to_csv("Reviews.csv", index=False)
-# f.close()
+review_df = pd.DataFrame(data={'IssuerID': idList, 'IssuerName': IssuerName, 'Reviews': comments})
+review_df.to_csv("C:\\Users\\satvi\\Documents\\DDDM\\Project\\Reviews.csv", index=False)
