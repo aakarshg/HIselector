5b2b3718ad3b2e4dd0533c3c4aa76558db4fa7b0
satvikshetty04 <satvikshetty04@gmail.com>
2017-11-17 14:26:22 -0500

    Updated hard filters function for pg1

--- Satvik/Plan_Similarity_Matching.py
+++ Satvik/Plan_Similarity_Matching.py
@@ -2,9 +2,13 @@ import sqlite3
 import pandas as pd
 import numpy as np
 from scipy.spatial.distance import euclidean
+import uszipcode
 
 # -------------------- User Preferences
 # State - NC
+# Zipcode - 27606
+# Age - 26
+# Tobacco Usage - No
 # Disease Management - Diabetes
 # Coinsurance - 20% (0)
 # Deductible IN - 200 (1)
@@ -19,20 +23,36 @@ def close_connection(conn, c):
     c.close()
     conn.close()
 
-def hard_filters(db_loc, state, disease):
+def hard_filters_pg1(db_loc, zip = None, age = None, tobacco_usage = None):
+    if zip == None or str(zip).strip() == "" or age == None or str(age).strip() == "" or tobacco_usage == None:
+        return "Enter all inputs"
+    search = uszipcode.ZipcodeSearchEngine()
+    state = None
+    if str(zip).strip() != "":
+        if search.by_zipcode(str(zip)):
+            state = search.by_zipcode(str(zip))['State']
+        else:
+            return "Invalid zipcode"
     conn, c = create_connection(db_loc)
-    print(("%" + disease + "%").title())
-    results = c.execute("SELECT * FROM PLAN_ATTRIBUTES where STATECODE = ? AND DISEASEMANAGEMENTPROGRAMSOFFERED LIKE ?",
-                        (state, "%" + disease + "%"))
+    # results = c.execute("SELECT * FROM PLAN_ATTRIBUTES where STATECODE = ? AND DISEASEMANAGEMENTPROGRAMSOFFERED LIKE ?",
+    #                     (state, "%" + disease + "%"))
+    # results = None
+    if tobacco_usage == "Yes":
+        results = c.execute("SELECT distinct plan_id, smoker_rate FROM cost where state = ? AND age_lower <= ? AND "
+                            "age_higher >= ? ", (state, age, age))
+    else:
+        results = c.execute("SELECT distinct plan_id, indiv_rate FROM cost where state = ? AND age_lower <= ? AND "
+                            "age_higher >= ? ", (state, age, age))
+
     if not results:
         print("No plans are available in your area covering your condition. Checking for closest match..")
         results = c.execute("SELECT * FROM PLAN_ATTRIBUTES where STATECODE = ?" , (state,))
 
-    df = pd.DataFrame(results.fetchall())
+    hard_df = pd.DataFrame(results.fetchall())
     print(results.description)
-    df.columns = [description[0] for description in results.description]
+    hard_df.columns = [description[0] for description in results.description]
     close_connection(conn, c)
-    return df
+    return hard_df
 
 
 def soft_filters(df, coin = None, ded_in = None, moop_in = None, ded_oon = None, moop_oon = None, international = None):
@@ -48,6 +68,6 @@ def soft_filters(df, coin = None, ded_in = None, moop_in = None, ded_oon = None,
     print(df.sort('distance').head())
 
 
-df = hard_filters("C:\\Users\\satvi\\Documents\\GitHub\\HIselector\\preprocessing\\sample.db", "NC", "DIAbetes")
+df = hard_filters_pg1("C:\\Users\\satvi\\Documents\\GitHub\\HIselector\\preprocessing\\sample.db", 27606, 26, 'No')
 print(len(df))
 soft_filters(df, 0, 1, 0)
